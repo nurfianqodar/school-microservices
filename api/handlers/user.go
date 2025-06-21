f35490c92564d22a2368159d4def0f99d809a9f7
nurfianqodar <77nurfianqodar@gmail.com>
2025-06-22 05:42:22 +0700

    create user list handler

--- api/handlers/user.go
+++ api/handlers/user.go
@@ -3,9 +3,11 @@ package handlers
 import (
 	"encoding/json"
 	"net/http"
+	"strconv"
 
 	pbusers "github.com/nurfianqodar/school-microservices/services/users/pb/users/v1"
 	"github.com/nurfianqodar/school-microservices/utils/httperr"
+	"github.com/nurfianqodar/school-microservices/utils/httpres"
 )
 
 type userHandler struct {
@@ -14,6 +16,7 @@ type userHandler struct {
 
 func (h *userHandler) RegisterRouter(mux *http.ServeMux) {
 	mux.HandleFunc("POST /api/v1/users/{$}", h.handleCreateOneUser)
+	mux.HandleFunc("GET /api/v1/users/{$}", h.handleCreateOneUser)
 }
 
 func NewUserHandler(s pbusers.UserServiceClient) Handler {
@@ -21,8 +24,8 @@ func NewUserHandler(s pbusers.UserServiceClient) Handler {
 }
 
 func (h *userHandler) handleCreateOneUser(w http.ResponseWriter, r *http.Request) {
+	// Read request body
 	defer r.Body.Close()
-
 	body := new(pbusers.CreateOneUserRequest)
 	err := json.NewDecoder(r.Body).Decode(body)
 	if err != nil {
@@ -30,11 +33,43 @@ func (h *userHandler) handleCreateOneUser(w http.ResponseWriter, r *http.Request
 		return
 	}
 
+	// Invoke service and get reponse
 	res, err := h.s.CreateOneUser(r.Context(), body)
 	if err != nil {
 		httperr.ConvertGRPCErrorToHTTPErr(err).Send(w)
 		return
 	}
-
+	// Send response
 	json.NewEncoder(w).Encode(res)
+}
+
+func (h *userHandler) handleListUser(w http.ResponseWriter, r *http.Request) {
+	takeQuery := r.URL.Query().Get("take")
+	skipQuery := r.URL.Query().Get("skip")
+
+	limit, err := strconv.Atoi(takeQuery)
+	if err != nil {
+		httperr.New(http.StatusBadRequest, "take query must be integer").Send(w)
+		return
+	}
+
+	offset, err := strconv.Atoi(skipQuery)
+	if err != nil {
+		httperr.New(http.StatusBadRequest, "skip query must be integer").Send(w)
+		return
+	}
+
+	if limit == 0 {
+		limit = 10
+	}
+
+	res, err := h.s.GetManyUser(r.Context(), &pbusers.GetManyUserRequest{
+		Limit:  uint64(limit),
+		Offset: uint64(offset),
+	})
+	if err != nil {
+		httperr.ConvertGRPCErrorToHTTPErr(err).Send(w)
+	}
+
+	json.NewEncoder(w).Encode(httpres.New(true, res))
 }
