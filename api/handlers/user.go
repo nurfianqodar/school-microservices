523b5d927879356013c2e88a8afcd3ad4c0bf863
nurfianqodar <77nurfianqodar@gmail.com>
2025-06-22 06:34:59 +0700

    login user api handler

--- api/handlers/user.go
+++ api/handlers/user.go
@@ -18,6 +18,8 @@ type userHandler struct {
 func (h *userHandler) RegisterRouter(mux *http.ServeMux) {
 	mux.HandleFunc("POST /api/v1/users/{$}", h.handleCreateOneUser)
 	mux.HandleFunc("GET /api/v1/users/{$}", h.handleListUser)
+
+	mux.HandleFunc("POST /api/v1/auth/login/{$}", h.handleLoginUser)
 }
 
 func NewUserHandler(s pbusers.UserServiceClient) Handler {
@@ -25,6 +27,7 @@ func NewUserHandler(s pbusers.UserServiceClient) Handler {
 }
 
 func (h *userHandler) handleCreateOneUser(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
 	// Read request body
 	defer r.Body.Close()
 	body := new(pbusers.CreateOneUserRequest)
@@ -46,6 +49,7 @@ func (h *userHandler) handleCreateOneUser(w http.ResponseWriter, r *http.Request
 
 func (h *userHandler) handleListUser(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
+
 	takeQuery := r.URL.Query().Get("take")
 	skipQuery := r.URL.Query().Get("skip")
 
@@ -65,9 +69,6 @@ func (h *userHandler) handleListUser(w http.ResponseWriter, r *http.Request) {
 		limit = 10
 	}
 
-	log.Println(limit)
-	log.Println(offset)
-
 	res, err := h.s.GetManyUser(r.Context(), &pbusers.GetManyUserRequest{
 		Limit:  uint64(limit),
 		Offset: uint64(offset),
@@ -76,5 +77,25 @@ func (h *userHandler) handleListUser(w http.ResponseWriter, r *http.Request) {
 		httperr.ConvertGRPCErrorToHTTPErr(err).Send(w)
 	}
 
+	json.NewEncoder(w).Encode(httpres.New(true, res))
+}
+
+func (h *userHandler) handleLoginUser(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	// read request body
+	defer r.Body.Close()
+	body := new(pbusers.LoginUserRequest)
+	if err := json.NewDecoder(r.Body).Decode(body); err != nil {
+		log.Println(err)
+		httperr.ErrInvalidRequestBody.Send(w)
+		return
+	}
+
+	res, err := h.s.LoginUser(r.Context(), body)
+	if err != nil {
+		log.Println(err)
+		httperr.ConvertGRPCErrorToHTTPErr(err).Send(w)
+	}
 	json.NewEncoder(w).Encode(httpres.New(true, res))
 }
