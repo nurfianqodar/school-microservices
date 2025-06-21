647b30708575df27031df0df6912f2f73c8985d4
nurfianqodar <77nurfianqodar@gmail.com>
2025-06-22 01:23:03 +0700

    create service login

--- services/users/services/services.go
+++ services/users/services/services.go
@@ -4,11 +4,13 @@ import (
 	"context"
 	"log"
 	"sync"
+	"time"
 
 	"github.com/go-playground/validator/v10"
 	"github.com/google/uuid"
 	"github.com/nurfianqodar/school-microservices/services/users/db"
 	pbusers "github.com/nurfianqodar/school-microservices/services/users/pb/users/v1"
+	"github.com/nurfianqodar/school-microservices/services/users/utils/token"
 	v "github.com/nurfianqodar/school-microservices/services/users/utils/validation"
 	"github.com/nurfianqodar/school-microservices/utils/errs"
 	"github.com/nurfianqodar/school-microservices/utils/hasher"
@@ -157,4 +159,47 @@ func (s *service) UpdateOneRoleUser(
 	req *pbusers.UpdateOneRoleUserRequest,
 ) (*pbusers.UpdateOneRoleUserResponse, error) {
 	panic("not implemented")
+}
+
+func (s *service) LoginUser(
+	ctx context.Context,
+	req *pbusers.LoginUserRequest,
+) (*pbusers.LoginUserResponse, error) {
+	// Count email
+	count, err := s.q.CountEmailUser(ctx, req.Email)
+	if err != nil {
+		log.Printf("error: failed to count user by email. %s", err.Error())
+		return nil, status.Error(codes.Internal, "internal server error")
+	}
+	if count != 1 {
+		return nil, errs.ErrInvalidCredential
+	}
+
+	// Get credential
+	creds, err := s.q.GetOneCredentialUserByEmail(ctx, req.Email)
+	if err != nil {
+		log.Printf("error: failed to get credential. %s", err.Error())
+		return nil, status.Error(codes.Internal, "internal server error")
+	}
+
+	// Compare password and hash
+	if err = hasher.CompareHashWithPassword(creds.PasswordHash, req.Password); err != nil {
+		return nil, err
+	}
+
+	// Create access and refresh token
+	// TODO fix audience
+	accessToken, err := token.CreateToken(token.TokenTypeAccess, creds.ID.String(), time.Minute*30, []string{"null"})
+	if err != nil {
+		return nil, err
+	}
+	refreshToken, err := token.CreateToken(token.TokenTypeRefresh, creds.ID.String(), time.Hour*24*30, []string{"null"})
+	if err != nil {
+		return nil, err
+	}
+
+	return &pbusers.LoginUserResponse{
+		AccessToken:  accessToken,
+		RefreshToken: refreshToken,
+	}, nil
 }
